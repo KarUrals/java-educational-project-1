1147548c42beec26177cee45b3c913267854c919
Karelskiy_Andrey <karelskiy.a@gmail.com>
2022-08-29 19:31:47 +0300

    refactoring Calc.java to increase readability

--- src/main/java/hexlet/code/games/Calc.java
+++ src/main/java/hexlet/code/games/Calc.java
@@ -14,22 +14,23 @@ public class Calc implements Games {
         int number1 = Engine.setNumber(1, 99);
         int number2 = Engine.setNumber(1, 99);
         int sign = Engine.setNumber(1, 3);
+        String operator = null;
 
         switch (sign) {
             case 1:
                 Engine.correctAnswer = String.valueOf(number1 + number2);
-                System.out.println("Question: " + number1 + " + " + number2);
+                operator = " + ";
                 break;
             case 2:
                 Engine.correctAnswer = String.valueOf(number1 - number2);
-                System.out.println("Question: " + number1 + " - " + number2);
+                operator = " - ";
                 break;
             case 3:
                 Engine.correctAnswer = String.valueOf(number1 * number2);
-                System.out.println("Question: " + number1 + " * " + number2);
+                operator = " * ";
                 break;
         }
-
+        System.out.println("Question: " + number1 + operator + number2);
         Engine.askPlayerAnswer();
         Engine.answerAnalyse(Engine.player, Engine.answer, Engine.correctAnswer);
     }
