d8b97f8abb96c6683bce343b82556be81d9bf30b
Karelskiy_Andrey <karelskiy.a@gmail.com>
2022-09-01 15:17:24 +0300

    reduce lines of code in Calc.java and Progression.java

--- src/main/java/hexlet/code/games/Calc.java
+++ src/main/java/hexlet/code/games/Calc.java
@@ -8,34 +8,29 @@ public class Calc {
 
     public static String[][] run() {
         String[][] array = new String[Engine.MAX_ROUND_NUMBER][2];
-        String correctAnswer = null;
 
-        int number1;
-        int number2;
-        String operator;
         for (String[] pair: array) {
-            number1 = Engine.setRandomNumber(1, Engine.MAX_RANDOM_NUMBER);
-            number2 = Engine.setRandomNumber(1, Engine.MAX_RANDOM_NUMBER);
-            operator = String.valueOf(Engine.setRandomNumber(1, NUMBER_OF_OPERATIONS));
+            int number1 = Engine.setRandomNumber(1, Engine.MAX_RANDOM_NUMBER);
+            int number2 = Engine.setRandomNumber(1, Engine.MAX_RANDOM_NUMBER);
+            String operator = String.valueOf(Engine.setRandomNumber(1, NUMBER_OF_OPERATIONS));
 
             switch (operator) {
                 case "1":
-                    correctAnswer = String.valueOf(number1 + number2);
+                    pair[1] = String.valueOf(number1 + number2);
                     operator = " + ";
                     break;
                 case "2":
-                    correctAnswer = String.valueOf(number1 - number2);
+                    pair[1] = String.valueOf(number1 - number2);
                     operator = " - ";
                     break;
                 case "3":
-                    correctAnswer = String.valueOf(number1 * number2);
+                    pair[1] = String.valueOf(number1 * number2);
                     operator = " * ";
                     break;
                 default:
                     break;
             }
             pair[0] = "Question: " + number1 + operator + number2;
-            pair[1] = correctAnswer;
         }
 
         return array;
--- src/main/java/hexlet/code/games/Progression.java
+++ src/main/java/hexlet/code/games/Progression.java
@@ -11,26 +11,21 @@ public class Progression {
         final int maxProgressionSize = 10;
 
         String[][] array = new String[Engine.MAX_ROUND_NUMBER][2];
-        int firstProgressionNumber;
-        int progressionStep;
-        int progressionSize;
-        int index;
 
-        String correctAnswer = null;
         String question;
 
         for (String[] pair: array) {
-            firstProgressionNumber = Engine.setRandomNumber(1, Engine.MAX_RANDOM_NUMBER);
-            progressionStep = Engine.setRandomNumber(-step, step);
-            progressionSize = Engine.setRandomNumber(minProgressionSize, maxProgressionSize);
-            index = Engine.setRandomNumber(0, progressionSize - 1);
+            int firstProgressionNumber = Engine.setRandomNumber(1, Engine.MAX_RANDOM_NUMBER);
+            int progressionStep = Engine.setRandomNumber(-step, step);
+            int progressionSize = Engine.setRandomNumber(minProgressionSize, maxProgressionSize);
+            int index = Engine.setRandomNumber(0, progressionSize - 1);
             int[] task = new int[progressionSize];
 
             question = "Question: ";
             for (int i = 0; i < progressionSize; i++) {
                 task[i] = firstProgressionNumber + progressionStep * i;
                 if (i == index) {
-                    correctAnswer = String.valueOf(task[i]);
+                    pair[1] = String.valueOf(task[i]);
                     question += ".. ";
                 } else {
                     question += task[i] + " ";
@@ -38,7 +33,6 @@ public class Progression {
             }
 
             pair[0] = question;
-            pair[1] = correctAnswer;
         }
 
         return array;
