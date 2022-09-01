bdf731f8634f0d8ff2fce3e6f6f5de80c6e588ef
Karelskiy_Andrey <karelskiy.a@gmail.com>
2022-09-01 15:20:12 +0300

    reduce lines of code in Prime.java

--- src/main/java/hexlet/code/games/Prime.java
+++ src/main/java/hexlet/code/games/Prime.java
@@ -7,20 +7,17 @@ public class Prime {
 
     public static String[][] run() {
         String[][] array = new String[Engine.MAX_ROUND_NUMBER][2];
-        int number;
-        String correctAnswer;
 
         for (String[] pair: array) {
-            number = Engine.setRandomNumber(2, Engine.MAX_RANDOM_NUMBER);
-            correctAnswer = "yes";
+            int number = Engine.setRandomNumber(2, Engine.MAX_RANDOM_NUMBER);
+            pair[1] = "yes";
             for (int i = 2; i <= Math.sqrt(number); i++) {
                 if (number % i == 0) {
-                    correctAnswer = "no";
+                    pair[1] = "no";
                     break;
                 }
             }
             pair[0] = "Question: " + number;
-            pair[1] = correctAnswer;
         }
 
         return array;
